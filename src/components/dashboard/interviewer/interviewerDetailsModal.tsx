079fa0c05116912b153f747440a4d7837fe1a8bc
hongyinzhuo-lgtm <hongyinzhuo@gmail.com>
2026-03-08 19:36:28 +0800

    Update interviewerDetailsModal.tsx

--- src/components/dashboard/interviewer/interviewerDetailsModal.tsx
+++ src/components/dashboard/interviewer/interviewerDetailsModal.tsx
@@ -17,7 +17,7 @@ function InterviewerDetailsModal({ interviewer }: Props) {
           <div className=" flex items-center justify-center border-4 overflow-hidden border-gray-500 rounded-xl h-48 w-44">
             <Image
               src={interviewer?.image || ""}
-              alt="Picture of the interviewer"
+              alt="面试官图片"
               width={180}
               height={30}
               className="w-full h-full object-cover object-center"
@@ -32,18 +32,18 @@ function InterviewerDetailsModal({ interviewer }: Props) {
             )}
           </div>
         </div>
-        <h3 className="text-mg m-0 p-0 mt-5 ml-0 font-medium">Interviewer Settings:</h3>
+        <h3 className="text-mg m-0 p-0 mt-5 ml-0 font-medium">面试官设置：</h3>
         <div className="flex flex-row space-x-14 justify-center items-start">
           <div className=" mt-2 flex flex-col justify-start items-start">
             <div className="flex flex-row justify-between items-center mb-2">
-              <h4 className="w-20 text-left">Empathy</h4>
+              <h4 className="w-20 text-left">同理心</h4>
               <div className="w-40 space-x-3 ml-3 flex justify-between items-center">
                 <Slider value={[(interviewer?.empathy || 10) / 10]} max={1} step={0.1} />
                 <span className="w-8 text-left">{(interviewer?.empathy || 10) / 10}</span>
               </div>
             </div>
             <div className="flex flex-row justify-between items-center ">
-              <h4 className="w-20 text-left">Rapport</h4>
+              <h4 className="w-20 text-left">亲和力</h4>
               <div className="w-40 space-x-3 ml-3 flex justify-between items-center">
                 <Slider value={[(interviewer?.rapport || 10) / 10]} max={1} step={0.1} />
                 <span className="w-8 text-left">{(interviewer?.rapport || 10) / 10}</span>
@@ -52,14 +52,14 @@ function InterviewerDetailsModal({ interviewer }: Props) {
           </div>
           <div className=" mt-2 flex flex-col justify-start items-start">
             <div className="flex flex-row justify-between items-center mb-2">
-              <h4 className="w-20 text-left">Exploration</h4>
+              <h4 className="w-20 text-left">探索度</h4>
               <div className="w-40 space-x-3 ml-3 flex justify-between items-center">
                 <Slider value={[(interviewer?.exploration || 10) / 10]} max={1} step={0.1} />
                 <span className="w-8 text-left">{(interviewer?.exploration || 10) / 10}</span>
               </div>
             </div>
             <div className="flex flex-row justify-between items-center ">
-              <h4 className="w-20 text-left">Speed</h4>
+              <h4 className="w-20 text-left">语速</h4>
               <div className="w-40 space-x-3 ml-3 flex justify-between items-center">
                 <Slider value={[(interviewer?.speed || 10) / 10]} max={1} step={0.1} />
                 <span className="w-8 text-left">{(interviewer?.speed || 10) / 10}</span>
