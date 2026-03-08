1f681502b5f66115c16a9763f678957b3a181aa3
hongyinzhuo-lgtm <hongyinzhuo@gmail.com>
2026-03-09 01:36:00 +0800

    Update pagefix.tsx

--- src/app/(client)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(client)/sign-in/[[...sign-in]]/page.tsx
@@ -7,15 +7,9 @@ function SignInPage() {
         <SignIn forceRedirectUrl="/dashboard" />
       </div>
       <div className="block md:hidden px-3 h-[60%] my-auto">
-        <h1 className="text-2xl font-bold text-center text-gray-800">
-          欢迎使用 ZhuoIN<span className="text-indigo-600">AI面试</span>
-        </h1>
-        <h1 className="text-md my-3 text-center text-gray-800">
-          移动端版本目前正在建设中。🚧
-        </h1>
-        <p className="text-center text-gray-600 mt-3">
-          为获得最佳体验，请使用电脑端登录。给您带来不便，敬请谅解。
-        </p>
+        <h1 className="text-2xl font-bold text-center text-gray-800">欢迎使用 ZhuoIN<span className="text-indigo-600">AI面试</span></h1>
+        <h1 className="text-md my-3 text-center text-gray-800">移动端版本目前正在建设中🚧</h1>
+        <p className="text-center text-gray-600 mt-3">为获得最佳体验，请使用电脑端登录。给您带来不便，敬请谅解</p>
       </div>
     </div>
   );
