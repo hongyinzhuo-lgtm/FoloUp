1aab25e1dcad753cdef3e264f3f5cc210fb2d070
hongyinzhuo-lgtm <hongyinzhuo@gmail.com>
2026-03-09 00:51:55 +0800

    Update singup page.tsx

--- src/app/(client)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(client)/sign-up/[[...sign-up]]/page.tsx
@@ -8,13 +8,13 @@ function SignUpPage() {
       </div>
       <div className="block md:hidden px-3 h-[60%] my-auto">
         <h1 className="text-2xl font-bold text-center text-gray-800">
-          Welcome to Folo<span className="text-indigo-600">Up</span>
+          欢迎使用 ZhuoIN<span className="text-indigo-600">AI面试</span>
         </h1>
         <h1 className="text-md my-3 text-center text-gray-800">
-          Mobile version is currently under construction. 🚧
+          移动端版本目前正在建设中。🚧
         </h1>
         <p className="text-center text-gray-600 mt-3">
-          Please sign in using a PC for the best experience. Sorry for the inconvenience.
+          为获得最佳体验，请使用电脑端注册或登录。给您带来不便，敬请谅解。
         </p>
       </div>
     </div>
